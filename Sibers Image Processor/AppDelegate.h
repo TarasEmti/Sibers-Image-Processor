d8096e203b200ba4f368f5c409630272b43e8b47
Taras Emti <t-min@mail.ru>
2018-03-05 22:47:19 +0700

    Initial Commit

---
+++ Sibers Image Processor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sibers Image Processor
+//
+//  Created by Тарас on 05.03.2018.
+//  Copyright © 2018 Taras Minin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
